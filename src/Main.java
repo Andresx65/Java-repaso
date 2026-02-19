97d7e8951694e7ba40d70da538d1c77322f2d05b
AndresG <andrespersonal2022@gmail.com>
2026-02-19 12:36:02 -0500

    Subir archivos

--- src/Main.java
+++ src/Main.java
@@ -2,3 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+ System.out.println("ANDRES_FELIPE_GUERRERO_GOMEZ");
+ System.out.println("Zipaquira");
+ System.out.println("18");
+System.out.println("Horus");
